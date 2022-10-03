e732801df83b570770a540927d07e832b7cd315a
Natalya <76129369+nwebcreator@users.noreply.github.com>
2022-10-03 17:14:43 +0300

    percentFormatter

---
+++ js/formatters.js
@@ -0,0 +1,6 @@
+export const percentFormatter = new Intl.NumberFormat('ru-RU', 
+    {
+        style: 'percent', 
+        maximumFractionDigits: 3
+    }
+); // 4,752 %
---
+++ js/programRates.js
@@ -0,0 +1,37 @@
+import {percentFormatter} from "./formatters.js";
+
+console.log(percentFormatter.format(0.567));
+
+// Ставки по ипотеке
+const programBase = 0.12;
+const programIt = 0.047;
+const programGov = 0.067;
+const programZero = 0.108;
+
+// Show program rates on page (Указываем ставку в радио кнопках)
+document.querySelector('#base-value').value = programBase;
+document.querySelector('#it-value').value = programIt;
+document.querySelector('#gov-value').value = programGov;
+document.querySelector('#zero-value').value = programZero;
+
+// Указываем ставку в label
+document.querySelector('#base-text').innerText = percentFormatter.format(programBase);
+document.querySelector('#it-text').innerText = percentFormatter.format(programIt);
+document.querySelector('#gov-text').innerText = percentFormatter.format(programGov);
+document.querySelector('#zero-text').innerText = percentFormatter.format(programZero);
+
+// Отображение выбранной процентной ставки - радиокнопки
+const programInputs = document.querySelectorAll('input[name="program"]');
+const totalPercent = document.querySelector('#total-percent');
+
+programInputs.forEach((input) => {
+    // Отображение ставки на старте
+    if (input.checked) {
+        totalPercent.innerText = percentFormatter.format(input.value);
+    };
+
+    //Отображение ставки при переключении
+    input.addEventListener('click', function () {
+        totalPercent.innerText = percentFormatter.format(this.value);
+    })
+})
